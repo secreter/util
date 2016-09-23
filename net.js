4b52e17b52b191a30fc31d785a0c6f01e8bfddb8
So <1979510177@qq.com>
2016-09-23 23:58:14 +0800

    xhr

---
+++ net.js
@@ -0,0 +1,58 @@
+ajax({
+	url:'http://redream.cn',
+	dataType:'json',
+	method:'GET',
+	data:{},
+	success:function(a,b){
+		console.log(a,b)
+	},
+	error:function(e){
+		console.log(e)
+	},
+})
+function ajax(option){
+	option=option||{}
+	option.method = (option.method || "GET").toUpperCase()
+	option.dataType=option.dataType||'json'
+	let param=formatParam(option.data)
+	let xhr=null
+	
+	//创建 - 非IE6 - 第一步
+	if(window.XMLHttpRequest){
+		xhr=new XMLHttpRequest()
+	}else{
+		//IE6及其以下版本浏览器
+		xhr = new ActiveXObject('Microsoft.XMLHTTP')
+	}
+	//接收 - 第三步
+	xhr.onreadystatechange=function(){
+		if (xhr.readystate == 4) {
+			let status = xhr.status
+			if (status >= 200 && status <300 ) {
+				option.success && option.success(xhr.responseText, xhr.responseXML)
+			} else {
+				option.error && option.error(status)
+			}
+		}
+	}
+	//连接 和 发送 - 第二步
+	if (option.method == 'GET') {
+		xhr.open('GET',option.url + '?' +param,true)
+		xhr.send(null)
+	}else{
+		xhr.open('POST',option.url,true)
+		//设置表单提交时的内容类型
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+    xhr.send(param)
+	}
+
+
+}
+function formatParam(obj){
+	let arr=[]
+	for(let key in obj){
+		arr.push(encodeURIComponent(key)+'='+encodeURIComponent(obj[key]))
+	}
+	arr.push(("v=" + Math.random()).replace(".",''))
+	return arr.join('&')
+}
---
+++ util.js
@@ -0,0 +1,21 @@
+//判断javascript类型
+function type(obj) {
+	var toString = Object.prototype.toString
+	var map = {
+	    '[object Boolean]'  : 'boolean', 
+	    '[object Number]'   : 'number', 
+	    '[object String]'   : 'string', 
+	    '[object Function]' : 'function', 
+	    '[object Array]'    : 'array', 
+	    '[object Date]'     : 'date', 
+	    '[object RegExp]'   : 'regExp', 
+	    '[object Undefined]': 'undefined',
+	    '[object Null]'     : 'null', 
+	    '[object Object]'   : 'object',
+	    '[object Map]'      : 'map',
+	    '[object Set]'      : 'set'
+	}
+	return map[toString.call(obj)]
+}
+
+//deepClone	
